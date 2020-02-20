06ed32106593421b294783fc03fc037fd6607492
MChernov <florenco_game@mail.ru>
2020-02-20 16:04:56 +0500

    works!

--- DZ_1/DZ_1_Task_5.py
+++ DZ_1/DZ_1_Task_5.py
@@ -2,14 +2,15 @@
 # Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят,
 # и сколько между ними находится букв.
 
-
+ID_BEGIN = 96
+ID_END = 123
 char_1 = input('Введите букву английского алфавита: ')
 char_2 = input('Введите другую букву английского алфавита этого же регистра: ')
 id_char_1 = ord(char_1)
 id_char_2 = ord(char_2)
-if  96 < id_char_1 < 123 and 96 < id_char_2 < 123 and id_char_1 != id_char_2:
-    id_char_1 = id_char_1 - 96
-    id_char_2 = id_char_2 - 96
+if ID_BEGIN < id_char_1 < ID_END and ID_BEGIN < id_char_2 < ID_END and id_char_1 != id_char_2:
+    id_char_1 = id_char_1 - ID_BEGIN
+    id_char_2 = id_char_2 - ID_BEGIN
     number_char = (abs(id_char_2 - id_char_1)) - 1
     print('Порядковый номер буквы "', char_1, '" в алфавите равен ', id_char_1)
     print('Порядковый номер буквы "', char_2, '" в алфавите равен ', id_char_2)
