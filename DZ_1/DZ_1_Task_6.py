de0ddf9b9d93a2a579b52c4b448ef4056c079a2b
MChernov <florenco_game@mail.ru>
2020-02-20 15:52:47 +0500

    works!

--- DZ_1/DZ_1_Task_6.py
+++ DZ_1/DZ_1_Task_6.py
@@ -1,2 +1,6 @@
-#
+# https://drive.google.com/file/d/1neKoHCH3e6r5qT4R3fEcjL3RNTTgKBW6/view?usp=sharing
 # Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+id_char = int(input('Введите номер буквы: '))
+char = chr(id_char + 96)
+print('Вы ввели код буквы "', char, '"')
